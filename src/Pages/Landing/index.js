72b4b9fcea39b9d38f08bb87c968749b48fa6b2d
mwahyusp <mwahyu73@gmail.com>
2021-08-05 22:41:24 +0700

    modify redux store

---
+++ src/Pages/Landing/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Navbar from "../../components/Navbar";
+import Style from "./style.module.css";
+
+function index() {
+  const handleClick = () => {
+    const Client_ID = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
+    const Response_Type = "token";
+    const Redirect_URI = "http://localhost:3000/";
+    const Scope = "playlist-modify-private";
+    window.location = `https://accounts.spotify.com/authorize?client_id=${Client_ID}&response_type=${Response_Type}&redirect_uri=${Redirect_URI}&scope=${Scope}&show_dialog=true`;
+  };
+
+  return (
+    <div>
+      <Navbar handleClick={handleClick} />
+      <div className={Style.container}>
+      </div>
+    </div>
+  );
+}
+
+export default index;
