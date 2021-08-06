4d5c8e5e45da74b27b7a6856ab71bfa37bd0eaf9
mwahyusp <mwahyu73@gmail.com>
2021-08-06 18:25:33 +0700

    adding some files

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,19 +1,20 @@
 import React from "react";
-<<<<<<< HEAD
 import { Link } from "react-router-dom";
-=======
-import Search from "../Search/Index";
->>>>>>> e1cefb1914cf7740000a3037d0c5e43d4a99379b
 import Navbar from "./style.module.css";
 
 function Index({ handleClick }) {
   return (
     <header>
       <Link className={Navbar.logo} to="/">
-        Music-App
+        J
+        <i className="fas fa-compact-disc" />
+        FY
       </Link>
       <div className={Navbar.navLeft}>
         <ul>
+          <li>Premium</li>
+          <li>About</li>
+          <li>Support</li>
           <li onClick={handleClick}>
             <div className={Navbar.btnAuth}>SIGN IN</div>
           </li>
